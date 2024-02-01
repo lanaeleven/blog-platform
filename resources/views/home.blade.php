6eb6c55213724061e321b8d838f00dd0617cd259
lanaeleven <maulanaelvn@gmail.com>
2024-02-01 21:01:16 +0800

    make authentication

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+    @auth
+    <div><a href="/dashboard">Dashboard</a></div>
+    <form action="/logout" method="post">
+        @csrf
+            <button type="submit">Logout</button>
+        </form>
+    @else
+    <div><a href="/login">Login</a></div>
+    @endauth
+    
+    <div class="flex justify-center items-center h-screen">
+        <div>
+            <h1 class="font-bold">Welcome to Blogizhopeedazh</h1>
+        </div>
+    </div>
+  
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/', [PostController::class, 'index']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/dashboard', function() {return view('dashboard');})->middleware('auth');
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
